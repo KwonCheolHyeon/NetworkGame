ea2f74922c3ab66e5db9d28232c6112469a6b5c4
KCheolHyeon <ch970314@naver.com>
2024-08-21 02:23:51 +0900

    네트워크 기본 세팅 완료

--- Assets/Script/Manager/NetworkManager.cs
+++ Assets/Script/Manager/NetworkManager.cs
@@ -5,6 +5,7 @@ using System;
 using System.Net.Sockets;
 using System.Text;
 using System.Numerics;
+using Goldmetal.UndeadSurvivor;
 
 
 public class NetworkManager : MonoBehaviour
@@ -33,6 +34,11 @@ public class NetworkManager : MonoBehaviour
     private NetworkStream stream;
     private int playerId;
 
+    private float mTransformX, mTransformY, mScaleX, mGunRotationZ;
+    private int mPlayerHp;
+    private bool bmIsShotOn;
+
+
     private void Start()
     {
         ConnectToServer();
@@ -43,25 +49,68 @@ public class NetworkManager : MonoBehaviour
         client = new TcpClient(IPandPort.MyIpAddress, IPandPort.MyPortNum);
         stream = client.GetStream();
 
-        // Receive player ID from server
         byte[] playerIdBytes = new byte[4];
         stream.Read(playerIdBytes, 0, playerIdBytes.Length);
         playerId = BitConverter.ToInt32(playerIdBytes, 0);
-        Debug.Log("Connected to server with player ID: " + playerId);
+        Debug.Log("연결 player ID: " + playerId);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        SendMovementData();
+
+        // Listen for data from server
+        if (stream.DataAvailable)
         {
-            SendMessageToServer("Player " + playerId + " shot!");
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string serverMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+            if (serverMessage.StartsWith("Player"))
+            {
+                //플레이어가 접속하면 정보를 받고 다른플레이어 소환 하는 매서드 작성 예정
+                Debug.Log(serverMessage); 
+            }
+            else
+            {
+              
+                ReceiveMovementData(buffer);
+            }
         }
     }
 
-    private void SendMessageToServer(string message)
+    private void SendMovementData()
     {
-        byte[] messageBytes = Encoding.ASCII.GetBytes(message);
-        stream.Write(messageBytes, 0, messageBytes.Length);
+        byte[] message = new byte[28];
+        Buffer.BlockCopy(BitConverter.GetBytes(playerId), 0, message, 0, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(mTransformX), 0, message, 4, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(mTransformY), 0, message, 8, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(mScaleX), 0, message, 12, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(mGunRotationZ), 0, message, 16, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(mPlayerHp), 0, message, 20, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(bmIsShotOn), 0, message, 24, 4);
+
+        stream.Write(message, 0, message.Length);
+    }
+
+    private void ReceiveMovementData(byte[] buffer)
+    {
+        int receivedPlayerId = BitConverter.ToInt32(buffer, 0);
+        float receivedX = BitConverter.ToSingle(buffer, 4);
+        float receivedY = BitConverter.ToSingle(buffer, 8);
+        float receivedScale = BitConverter.ToSingle(buffer, 12);
+        float gunRotationZ = BitConverter.ToSingle(buffer, 16);
+        int playerHp = BitConverter.ToInt32(buffer, 20);
+        bool mIsShotOn = BitConverter.ToBoolean(buffer, 24);
+
+
+        UpdateOtherPlayerPosition(receivedPlayerId, receivedX, receivedY, receivedScale, gunRotationZ, playerHp, mIsShotOn);
+
+    }
+
+    private void UpdateOtherPlayerPosition(int playerId, float transformX, float transformY, float scaleX, float gunRotationZ, int playerHp, bool mIsShotOn)
+    {
+        //playerId를 통해 저장된 각 오브젝트들을 동기화 할 예정
     }
 
     private void OnApplicationQuit()
